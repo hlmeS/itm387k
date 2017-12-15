b3693be9e0514852401d54b8a9d795a100a99cdc
Holm Smidt <hsmidt@hawaii.edu>
2017-12-15 06:16:13 -1000

    test file

---
+++ python/energyAnalytics/test.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd
+import matplotlib as plt 
+import datetime
+import mysql.connector
+from mysql.connector import MySQLConnection, Error
+
+print "Yeah, I have all packages installed already."
